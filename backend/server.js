7107a8284711129c1c2a2660388d442094aec37b
iamfannyhenriques <fanny.henriques@hotmail.com>
2025-01-10 12:49:17 +0100

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,42 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const playgroundSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  location: {
+    type: {
+      type: String, // GeoJSON type
+      default: "Point",
+    },
+    coordinates: [Number], // [longitude, latitude]
+  },
+  address: { type: String },
+  source: { type: String, enum: ['Stockholm', 'Google'], required: true },
+  facilities: [String], // e.g., ["Swings", "Slides"]
+  images: [String], // URLs for images
+  ratings: { type: Number }, // From Google Places
+  googlePlaceId: { type: String }, // Optional for Google data
+});
+
+module.exports = mongoose.model('Playground', playgroundSchema);
+
+//Installera axios/fetch 
+//
 
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
@@ -20,3 +46,27 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
+// const mongoose = require('mongoose');
+
+// const playgroundSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   description: { type: String },
+//   location: {
+//     type: {
+//       type: String, // GeoJSON type
+//       default: "Point",
+//     },
+//     coordinates: [Number], // [longitude, latitude]
+//   },
+//   address: { type: String },
+//   source: { type: String, enum: ['Stockholm', 'Google'], required: true },
+//   facilities: [String], // e.g., ["Swings", "Slides"]
+//   images: [String], // URLs for images
+//   ratings: { type: Number }, // From Google Places
+//   googlePlaceId: { type: String }, // Optional for Google data
+// });
+
+// module.exports = mongoose.model('Playground', playgroundSchema);
