6d4faef638ad9a3b0238418e2796ada370af6774
iamfannyhenriques <fanny.henriques@hotmail.com>
2025-01-12 22:20:18 +0100

    added model for user, and route for registration, login and to post a playground once logged in

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import axios from "axios";
+import crypto from "crypto";
+import bcrypt from "bcrypt-nodejs"
 
 dotenv.config();
 
@@ -16,6 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//SCHEMA AND MODELS
 const playgroundSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
@@ -27,16 +30,52 @@ const playgroundSchema = new mongoose.Schema({
     coordinates: [Number], //longitude, latitude
   },
   address: { type: String },
-  source: { type: String, enum: ['Stockholm', 'Google'], required: true },
+  source: { type: String, enum: ['Stockholm', 'Google'] },
   facilities: [String], // ex. "Swings", "Slides" 
   images: [String], // URL
   ratings: { type: Number },
   googlePlaceId: { type: String },
+  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 });
 
 //exporting named export for flexibility instead of default
 export const Playground = mongoose.model('Playground', playgroundSchema);
 
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+//middleware for authentication
+const authenticateUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ accessToken: req.header("Authorization") })
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(401).json({ loggedOut: true, message: "Invalid access token" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+//GET-ROUTES
 app.get("/", (req, res) => {
   res.send("This is the playground app!");
 });
@@ -51,11 +90,13 @@ app.get("/playgrounds", async (req, res) => {
   // Query param for name - the playground name or the neighborhood name (ex. vasaparken or vasaparken playground)
   if (name) {
     apiUrl = `${process.env.GOOGLE_PLACES_URL}?query=playground+in+${name}&key=${process.env.GOOGLE_API_KEY}`;
-    // apiUrl += `&query=${name}`;
   }
-  if (location) { //This might not be nessecary unless we want to find an exact location using coordinates
-    apiUrl = `${process.env.GOOGLE_PLACES_URL}?location=${location}&query=playground&key=${process.env.GOOGLE_API_KEY}`;
-    // apiUrl += `&location=${location}`;
+  // changed to postal code?
+  if (location) {
+    const [lat, lng] = location.split(',').map(coord => coord.trim());
+    const radius = 100;
+    // Constructing the API URL with location and radius
+    apiUrl = `${process.env.GOOGLE_PLACES_URL}?location=${lat},${lng}&radius=${radius}&types=playground&key=${process.env.GOOGLE_API_KEY}`;
   }
   // Fetching data using axios
   try {
@@ -93,6 +134,79 @@ app.get("/playgrounds/id/:place_id", async (req, res) => {
   }
 });
 
+//ROUTES TO REGISTER, LOGIN AND POST
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = new User({ name, email, password: bcrypt.hashSync(password) });
+    await user.save();
+    res.status(201).json({
+      id: user._id,
+      accessToken: user.accessToken,
+      message: "User registered successfully"
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Failed to register user",
+      error: error.message,
+    });
+  }
+});
+
+//route for login
+app.post("/login", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const user = await User.findOne({
+      $or: [{ name }, { email }]  // $or - make it possible to use both name or email 
+    });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        userId: user._id,
+        accessToken: user.accessToken,
+        message: "Logged in successfully"
+      });
+    } else {
+      res.status(401).json({
+        success: false,
+        notFound: true,
+        message: "Invalid credentials"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message
+    });
+  }
+});
+
+
+//route to post a playground
+app.post("/playgrounds", authenticateUser, async (req, res) => {
+  const { name, description, address, facilities, images } = req.body;
+
+  try {
+    const newPlayground = new Playground({
+      name,
+      description,
+      address,
+      facilities,
+      images,
+      postedBy: req.user._id
+    });
+    await newPlayground.save();
+    res.status(201).json({ success: true, message: "Playground created successfully", playground: newPlayground });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal server error", error });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
