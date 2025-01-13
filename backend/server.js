213c8d22209a7b5ea08424a9ad60df8e5de4922e
Anna <annah80@gmail.com>
2025-01-13 13:36:56 +0100

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const playgroundRoutes = require("./routes/playgrounds");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import axios from "axios";
+import crypto from "crypto";
+import bcrypt from "bcrypt-nodejs";
+import playgroundRoutes from "./routes/playground-routes.js";
 
 dotenv.config();
 
+const mongoUrl =
+  process.env.MONGO_URL || "mongodb://localhost/project-playground";
+mongoose.connect(mongoUrl);
+mongoose.Promise = Promise;
+
+const port = process.env.PORT || 9000;
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 // Middleware
 app.use(cors()); // Enable CORS for all requests
@@ -34,7 +46,6 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
