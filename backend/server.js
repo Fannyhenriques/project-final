618dd5d752019fb3df552ce2f320f137d00d0345
Anna <annah80@gmail.com>
2025-01-13 13:39:12 +0100

    small update to server.js

--- backend/server.js
+++ backend/server.js
@@ -47,5 +47,5 @@ app.get("/", (req, res) => {
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
