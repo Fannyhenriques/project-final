8f86f95a9482448b7bfb214033af724194a79066
Anna <annah80@gmail.com>
2025-01-13 15:45:46 +0100

    updated server.js test.js files

--- backend/server.js
+++ backend/server.js
@@ -20,25 +20,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-//middleware for authentication
-const authenticateUser = async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      accessToken: req.header("Authorization"),
-    });
-    if (user) {
-      req.user = user;
-      next();
-    } else {
-      res
-        .status(401)
-        .json({ loggedOut: true, message: "Invalid access token" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
-  }
-};
-
 // Use the playground routes
 app.use("/api/playgrounds", playgroundRoutes); // Routes for playgrounds
 
