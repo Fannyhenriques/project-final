54e2e3123d6e2bba490fcca73246c70a436b093d
Anna <annah80@gmail.com>
2025-01-30 15:48:09 +0100

    changed to a for loop in the maploader to debug why the markers are not visible when deployed

--- frontend/src/hooks/getUserLocation.jsx
+++ frontend/src/hooks/getUserLocation.jsx
@@ -21,34 +21,3 @@ export const getUserLocation = async () => {
     }
   });
 };
-
-
-
-// window.onload = () => {
-//   getUserLocation();
-// };
-
-// export const getUserLocation = async () => {
-//   const fallbackCoordinates = { lat: 59.3293, lng: 18.0686 }; // Stockholm
-
-//   return new Promise((resolve) => {
-//     if ("geolocation" in navigator) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           resolve({
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude,
-//           });
-//         },
-//         () => {
-//           console.warn("Geolocation denied, using fallback.");
-//           resolve(fallbackCoordinates);
-//         }
-//       );
-//     } else {
-//       console.warn("Geolocation not supported, using fallback.");
-//       resolve(fallbackCoordinates);
-//     }
-//   });
-// };
-
